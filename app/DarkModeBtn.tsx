f8496fe5cd97097f6819da14b6b1598e24110e6d
ProskurinV <proskyrinv.g@gmail.com>
2023-03-27 10:32:45 +0300

    darkMode

---
+++ app/DarkModeBtn.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
+
+function DarkModeBtn() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme, systemTheme } = useTheme();
+
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  const currentTheme = theme === "system " ? systemTheme : theme;
+
+  return (
+    <div className="flex justify-end">
+      <select value={theme} onChange={(e) => setTheme(e.target.value)}>
+        <option value="system">System</option>
+        <option value="dark">Dark</option>
+        <option value="light">Light</option>
+      </select>
+
+      {currentTheme === "dark" ? (
+        <BsFillMoonFill
+          className=" h-8 w-8 cursor-pointer"
+          onClick={() => {
+            setTheme("light");
+          }}
+        />
+      ) : (
+        <BsFillSunFill
+          className="h-8 w-8 cursor-pointer"
+          onClick={() => {
+            setTheme("dark");
+          }}
+        />
+      )}
+    </div>
+  );
+}
+export default DarkModeBtn;
